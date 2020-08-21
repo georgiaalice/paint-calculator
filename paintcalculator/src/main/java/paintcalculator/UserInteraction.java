736b2b3f67ccc372479c52d2ddcd293b1e13b667
Georgia M <georgia63605@gchq.gov.uk>
2020-08-21 15:10:01 -0700

    Refactored code out of App.java

---
+++ paintcalculator/src/main/java/paintcalculator/UserInteraction.java
@@ -0,0 +1,31 @@
+package paintcalculator;
+
+import java.util.Scanner;
+
+public class UserInteraction {
+    public static void calculateOutputs() {
+        Calculations calculations = new Calculations();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the width of the room in metres:");
+        int width = in.nextInt();
+
+        System.out.println("Enter the length of the room in metres:");
+        int length = in.nextInt();
+
+        System.out.println("Enter the height of the room in metres:");
+        int height = in.nextInt();
+
+        in.close();
+
+        int floorArea = calculations.calculateAreaOfFloor(width, length);
+        int paintRequired = calculations.calculateAmountOfPaintRequired(width, length, height);
+        int volumeOfRoom = calculations.calculateVolumeOfRoom(width, length, height);
+
+        System.out.println("Area of the floor: " + floorArea + " metres squared");
+        System.out.println("Amount of paint required to paint the walls: " + paintRequired + " litres");
+        System.out.println("Volume of the room: " + volumeOfRoom + " metres cubed");
+
+    }
+
+}
